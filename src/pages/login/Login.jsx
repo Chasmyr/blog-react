139b361d957fef09bef91ac68ec52eb8437f9c3d
Chasmyr <lestratgabin5886@gmail.com>
2022-02-04 15:15:02 +0100

    register & login page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+      <div className="login">
+          <span className="login-title">Login</span>
+          <form className="login-form">
+              <label>Email</label>
+              <input type="text" placeholder="Enter your email..." className="login-input"/>
+              <label>Password</label>
+              <input type="password" className="login-input" placeholder="Enter your password..."/>
+              <button className="login-button">Login</button>
+          </form>
+          <button className="login-register">Register</button>
+      </div>
+  )
+}
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,19 @@
+import "./register.css";
+
+export default function Register() {
+  return (
+      <div className="register">
+          <span className="register-title">Register</span>
+          <form className="register-form">
+                <label>Username</label>
+                <input type="text" className="register-input" placeholder="Enter your username..."/>
+                <label>Email</label>
+                <input type="text" placeholder="Enter your email..." className="register-input"/>
+                <label>Password</label>
+                <input type="password" className="register-input" placeholder="Enter your password..."/>
+                <button className="register-button">Register</button>
+          </form>
+          <button className="register-login">Register</button>
+      </div>
+  )
+}
